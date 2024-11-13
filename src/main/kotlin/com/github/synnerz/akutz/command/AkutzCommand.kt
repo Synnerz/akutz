1790300d103a35b824ac6a7b18f676521e9f30fb
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-12 21:39:46 -0500

    AkutzCommand: fix out of bounds on empty args, add "help", and unload before loading again

--- src/main/kotlin/com/github/synnerz/akutz/command/AkutzCommand.kt
+++ src/main/kotlin/com/github/synnerz/akutz/command/AkutzCommand.kt
@@ -8,15 +8,25 @@ import net.minecraft.util.ChatComponentText
 
 object AkutzCommand : BaseCommand("Akutz", listOf("akutz", "az", "akz")) {
     override fun processCommand(player: EntityPlayerSP, args: Array<String>) {
-        if (args[0] == "load") {
-            Impl.setup()
-            ModuleManager.setup()
-            player.addChatMessage(ChatComponentText("Successfully loaded akutz"))
-            return
-        } else if (args[0] == "unload") {
-            Impl.clear()
-            player.addChatMessage(ChatComponentText("Successfully unloaded akutz"))
-            return
+        if (args.isEmpty()) return showHelp(player)
+        when (args[0]) {
+            "load", "reload" -> {
+                Impl.clear()
+                Impl.setup()
+                ModuleManager.setup()
+                player.addChatMessage(ChatComponentText("Successfully loaded akutz"))
+            }
+            "unload" -> {
+                Impl.clear()
+                player.addChatMessage(ChatComponentText("Successfully unloaded akutz"))
+            }
+            else -> player.addChatMessage(ChatComponentText("Unknown argument " + args[0]))
         }
+    }
+
+    private fun showHelp(player: EntityPlayerSP) {
+        player.addChatMessage(ChatComponentText("Akutz: the BETTER minecraft javascript bindings"))
+        player.addChatMessage(ChatComponentText("/akutz load"))
+        player.addChatMessage(ChatComponentText("/akutz unload"))
     }
 }
