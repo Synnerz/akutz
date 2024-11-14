1612a62f40d980df1907870d958db7223e13f9f8
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-13 19:31:18 -0700

    api: added `EventTrigger`

---
+++ src/main/kotlin/com/github/synnerz/akutz/api/events/EventTrigger.kt
@@ -0,0 +1,68 @@
+package com.github.synnerz.akutz.api.events
+
+import com.github.synnerz.akutz.engine.impl.Impl
+import com.github.synnerz.akutz.engine.impl.Loader
+
+abstract class EventTrigger protected constructor(
+    var method: (args: Array<out Any?>) -> Unit,
+    var type: EventType
+) : Comparable<EventTrigger> {
+    private var registered: Boolean = false
+    private var priority: Priority = Priority.NORMAL
+
+    init {
+        @Suppress("LeakingThis")
+        register()
+    }
+
+    fun setPriority(prio: Priority) = apply {
+        this.priority = prio
+
+        unregister()
+        register()
+    }
+
+    open fun register() = apply {
+        if (registered) return this
+
+        registered = true
+        Loader.addEvent(this)
+    }
+
+    open fun unregister() = apply {
+        if (!registered) return this
+
+        registered = false
+        Loader.removeEvent(this)
+    }
+
+    open fun isRegistered() : Boolean = registered
+
+    abstract fun trigger(args: Array<out Any?>)
+
+    fun callMethod(args: Array<out Any?>) {
+        if (!Impl.isLoaded()) return
+
+        try {
+            method(args)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun compareTo(other: EventTrigger): Int {
+        val ordCmp = priority.ordinal - other.priority.ordinal
+        return if (ordCmp == 0)
+            hashCode() - other.hashCode()
+        else ordCmp
+    }
+
+    enum class Priority {
+        //LOWEST IS RAN LAST
+        HIGHEST,
+        HIGH,
+        NORMAL,
+        LOW,
+        LOWEST
+    }
+}
---
+++ src/main/kotlin/com/github/synnerz/akutz/api/events/EventType.kt
@@ -0,0 +1,72 @@
+package com.github.synnerz.akutz.api.events
+
+/**
+ * Taken from ChatTriggers under MIT License
+ * [Link](https://github.com/ChatTriggers/ChatTriggers/blob/master/src/main/kotlin/com/chattriggers/ctjs/triggers/TriggerType.kt)
+ */
+enum class EventType {
+    // Client
+    Chat,
+    ActionBar,
+    Tick,
+    Step,
+    GameUnload,
+    GameLoad,
+    Clicked,
+    Scrolled,
+    Dragged,
+    GuiOpened,
+    PickupItem,
+    DropItem,
+    MessageSent,
+    Tooltip,
+    PlayerInteract,
+    AttackEntity,
+    HitBlock,
+    GuiRender,
+    GuiKey,
+    GuiMouseClick,
+    GuiMouseRelease,
+    GuiMouseDrag,
+    PacketSent,
+    PacketReceived,
+    ServerConnect,
+    ServerDisconnect,
+    GuiClosed,
+    RenderSlot,
+
+    // Rendering
+    RenderWorld,
+    BlockHighlight,
+    RenderOverlay,
+    RenderChat,
+    RenderScoreboard,
+    RenderTitle,
+    RenderEntity,
+    PostGuiRender,
+    PreItemRender,
+    RenderItemIntoGui,
+    RenderItemOverlayIntoGui,
+    RenderSlotHighlight,
+    PostRenderEntity,
+    RenderTileEntity,
+    PostRenderTileEntity,
+
+    // World
+    SoundPlay,
+    WorldLoad,
+    WorldUnload,
+    BlockBreak,
+    SpawnParticle,
+    EntityDeath,
+    EntityDamage,
+
+    // Misc
+    Forge,
+    Command,
+    Other;
+
+    fun triggerAll(vararg args: Any?) {
+//        ModuleManager.trigger(this, args)
+    }
+}
