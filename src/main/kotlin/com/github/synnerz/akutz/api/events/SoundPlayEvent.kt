75fb1913e945ffc7a9d4eeec87adca3f76d008c4
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-15 22:35:15 -0700

    impl: add `SoundPlayEvent`

---
+++ src/main/kotlin/com/github/synnerz/akutz/api/events/SoundPlayEvent.kt
@@ -0,0 +1,25 @@
+package com.github.synnerz.akutz.api.events
+
+/**
+ * Taken from ChatTriggers under MIT License
+ * [Link](https://github.com/ChatTriggers/ChatTriggers/blob/master/src/main/kotlin/com/chattriggers/ctjs/triggers/SoundPlayTrigger.kt)
+ */
+class SoundPlayEvent(
+    method: (args: Array<out Any?>) -> Unit
+) : EventTrigger(method, EventType.SoundPlay) {
+    private var soundCriteria: String? = null
+
+    fun setCriteria(criteria: String) = apply {
+        this.soundCriteria = criteria
+    }
+
+    override fun trigger(args: Array<out Any?>) {
+        if (
+            args[1] is String &&
+            soundCriteria != null &&
+            !(args[1] as String).equals(soundCriteria, ignoreCase = true)
+            ) return
+
+        callMethod(args)
+    }
+}
