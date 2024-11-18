fc9e0ca21443d9e7317bafaf56de2a3cbfb1d8ad
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-17 23:39:34 -0700

    backend: add `PlayerInteract` listener

--- src/main/kotlin/com/github/synnerz/akutz/listeners/ClientListener.kt
+++ src/main/kotlin/com/github/synnerz/akutz/listeners/ClientListener.kt
@@ -2,11 +2,13 @@ package com.github.synnerz.akutz.listeners
 
 import com.github.synnerz.akutz.api.events.EventType
 import com.github.synnerz.akutz.api.wrappers.World
+import net.minecraft.util.BlockPos
 import net.minecraftforge.client.event.DrawBlockHighlightEvent
 import net.minecraftforge.client.event.GuiOpenEvent
 import net.minecraftforge.client.event.GuiScreenEvent
 import net.minecraftforge.client.event.GuiScreenEvent.DrawScreenEvent
 import net.minecraftforge.client.event.RenderGameOverlayEvent
+import net.minecraftforge.event.entity.player.PlayerInteractEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent
 import org.lwjgl.util.vector.Vector3f
@@ -63,5 +65,27 @@ object ClientListener {
         )
 
         EventType.BlockHighlight.triggerAll(vec, event)
+    }
+
+    @SubscribeEvent
+    fun onInteract(event: PlayerInteractEvent) {
+        val action: String = when(event.action) {
+            PlayerInteractEvent.Action.LEFT_CLICK_BLOCK -> return
+            PlayerInteractEvent.Action.RIGHT_CLICK_AIR -> "RIGHT_CLICK_EMPTY"
+            PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK -> "RIGHT_CLICK_BLOCK"
+            null -> "UNKNOWN"
+        }
+
+        val pos = event.pos ?: BlockPos(0, 0, 0)
+
+        EventType.PlayerInteract.triggerAll(
+            action,
+            Vector3f(
+                pos.x.toFloat(),
+                pos.y.toFloat(),
+                pos.z.toFloat()
+            ),
+            event
+        )
     }
 }
