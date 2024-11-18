a92932dd0556a63f3bfc5f111efe9525dc883bca
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-17 19:19:06 -0700

    api: finish `TileEntity` wrapper

--- src/main/kotlin/com/github/synnerz/akutz/api/wrappers/entity/TileEntity.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/wrappers/entity/TileEntity.kt
@@ -1,12 +1,27 @@
 package com.github.synnerz.akutz.api.wrappers.entity
 
+import com.github.synnerz.akutz.api.wrappers.world.block.Block
+import net.minecraft.util.BlockPos
 import net.minecraft.tileentity.TileEntity as MCTileEntity
 
 /**
  * Taken from ChatTriggers under MIT License
  * [Link](https://github.com/ChatTriggers/ChatTriggers/blob/master/src/main/kotlin/com/chattriggers/ctjs/minecraft/wrappers/entity/TileEntity.kt)
  */
-// TODO: finish once Block wrapper is done
 class TileEntity(val tileEntity: MCTileEntity) {
+    fun getX(): Int = tileEntity.pos.x
+
+    fun getY(): Int = tileEntity.pos.y
+
+    fun getZ(): Int = tileEntity.pos.z
+
+    fun getBlock(): Block = Block(tileEntity.blockType, tileEntity.pos)
+
+    fun getBlockPos(): BlockPos = tileEntity.pos
+
+    fun getMetadata(): Int = tileEntity.blockMetadata
+
     fun toMC(): MCTileEntity = tileEntity
+
+    override fun toString(): String = "TileEntity{x=${getX()}, y=${getY()}, z=${getZ()}, block=${getBlock()}}"
 }
