c32a50527e298fd461c2809be7c69c7d8780c901
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-17 17:53:34 -0700

    backend: add `Entity` wrapper

---
+++ src/main/kotlin/com/github/synnerz/akutz/api/wrappers/entity/Entity.kt
@@ -0,0 +1,66 @@
+package com.github.synnerz.akutz.api.wrappers.entity
+
+import com.github.synnerz.akutz.api.wrappers.world.Chunk
+import net.minecraft.world.World
+import java.util.UUID
+import net.minecraft.entity.Entity as MCEntity
+
+open class Entity(val entity: MCEntity) {
+    fun getX(): Double = entity.posX
+
+    fun getY(): Double = entity.posY
+
+    fun getZ(): Double = entity.posZ
+
+    fun getLastX(): Double = entity.lastTickPosX
+
+    fun getLastY(): Double = entity.lastTickPosY
+
+    fun getLastZ(): Double = entity.lastTickPosZ
+
+    fun isDead(): Boolean = entity.isDead
+
+    fun getWidth(): Float = entity.width
+
+    fun getHeight(): Float = entity.height
+
+    fun getEyeHeight(): Float = entity.eyeHeight
+
+    fun getName(): String = entity.name
+
+    fun getUUID(): String = getUUIDObj().toString()
+
+    fun getUUIDObj(): UUID = entity.uniqueID
+
+    fun distanceTo(other: Entity): Float = distanceTo(other.entity)
+
+    fun distanceTo(other: MCEntity): Float = entity.getDistanceToEntity(other)
+
+    // TODO: blockpos distanceTo
+
+    fun distanceTo(x: Float, y: Float, z: Float): Float = entity.getDistance(
+        x.toDouble(), y.toDouble(), z.toDouble()
+    ).toFloat()
+
+    fun distanceTo(x: Double, y: Double, z: Double): Double = entity.getDistance(
+        x, y, z
+    )
+
+    fun isOnGround(): Boolean = entity.onGround
+
+    fun isCollided(): Boolean = entity.isCollided
+
+    fun getTicksExisted(): Int = entity.ticksExisted
+
+    fun isSneaking(): Boolean = entity.isSneaking
+
+    fun isSprinting(): Boolean = entity.isSprinting
+
+    fun getWorld(): World = entity.entityWorld
+
+    fun getChunk(): Chunk = Chunk(
+        getWorld().getChunkFromChunkCoords(entity.chunkCoordX, entity.chunkCoordZ)
+    )
+
+    fun toMC(): MCEntity = entity
+}
