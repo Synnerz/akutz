dc040cc0956ca444935c58ae37e0e3524c2bb8c0
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-17 17:54:09 -0700

    backend: add `Chunk` wrapper

---
+++ src/main/kotlin/com/github/synnerz/akutz/api/wrappers/world/Chunk.kt
@@ -0,0 +1,33 @@
+package com.github.synnerz.akutz.api.wrappers.world
+
+import com.github.synnerz.akutz.api.wrappers.entity.Entity
+import com.github.synnerz.akutz.api.wrappers.entity.TileEntity
+import net.minecraft.world.chunk.Chunk as MCChunk
+
+class Chunk(val chunk: MCChunk) {
+    fun getX(): Int = chunk.xPosition
+
+    fun getZ(): Int = chunk.zPosition
+
+    fun getMinBlockX(): Int = getX() * 16
+
+    fun getMinBlockZ(): Int = getZ() * 16
+
+    fun getAllEntities(): List<Entity> {
+        return chunk.entityLists.toList().flatten().map(::Entity)
+    }
+
+    fun getAllEntitiesOfType(clazz: Class<*>): List<Entity> {
+        return getAllEntities().filter { clazz.isInstance(it.entity) }
+    }
+
+    fun getAllTileEntities(): List<TileEntity> {
+        return chunk.tileEntityMap.values.map(::TileEntity)
+    }
+
+    fun getAllTileEntitiesOfType(clazz: Class<*>): List<TileEntity> {
+        return getAllTileEntities().filter { clazz.isInstance(it.tileEntity) }
+    }
+
+    fun toMC(): MCChunk = chunk
+}
