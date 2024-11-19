7d37c1f3db7c72e7359133cc3a11ac23152649b3
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-19 15:49:16 -0500

    ChatLib: add a few methods

--- src/main/kotlin/com/github/synnerz/akutz/api/libs/ChatLib.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/libs/ChatLib.kt
@@ -1,6 +1,9 @@
 package com.github.synnerz.akutz.api.libs
 
+import com.github.synnerz.akutz.api.libs.render.Renderer
+import com.github.synnerz.akutz.api.wrappers.Client
 import com.github.synnerz.akutz.api.wrappers.Player
+import net.minecraft.client.Minecraft
 import net.minecraft.util.ChatComponentText
 import net.minecraftforge.client.ClientCommandHandler
 import net.minecraftforge.client.event.ClientChatReceivedEvent
@@ -51,4 +54,81 @@ object ChatLib {
     @JvmStatic
     fun getChatMessage(event: ClientChatReceivedEvent, formatted: Boolean = false): String =
         if (formatted) replaceFormatting(event.message.formattedText) else event.message.unformattedText
+
+    @JvmStatic
+    fun relativeTimeToString(t: Double): String {
+        var time = t / 1000
+        if (time < 60) return "${time.toInt()}s"
+        if (time < 60 * 5) {
+            val minutes = (time / 60).toInt()
+            val seconds = (time % 60).toInt()
+            return "${minutes}m ${seconds}s"
+        }
+        time /= 60
+        if (time < 60) return "${time.toInt()}m"
+        if (time < 60 * 5) {
+            val hours = (time / 60).toInt()
+            val minutes = (time % 60).toInt()
+            return "${hours}h ${minutes}m"
+        }
+        time /= 60
+        if (time < 24) return "${time.toInt()}h"
+        if (time < 48) {
+            val hours = (time - 24).toInt()
+            return "1d ${hours}h"
+        }
+        time /= 24
+        if (time < 7) return "${time.toInt()}d"
+        val years = time / 365
+        time %= 365
+        val weeks = time / 7
+        time %= 7
+
+        val yearStr = if (years >= 1) "${years.toInt()}y " else ""
+        val weekStr = if (weeks >= 1) "${weeks.toInt()}w " else ""
+        val dayStr = if (time >= 1) "${time.toInt()}d" else ""
+
+        return "$yearStr$weekStr$dayStr".trim()
+    }
+
+    @JvmStatic
+    @JvmOverloads
+    fun addCommas(value: Number, decimalPlaces: Int = 0): String = "%,.${decimalPlaces}f".format(value)
+
+    @JvmStatic
+    @JvmOverloads
+    fun colorForNumber(value: Double, max: Double = 1.0): String {
+        if (value > max * 0.75) return "§2"
+        if (value > max * 0.50) return "§e"
+        if (value > max * 0.25) return "§6"
+        return "§4"
+    }
+
+    @JvmStatic
+    fun getChatWidth() = Client.getChatGui()?.chatWidth ?: 0
+
+    @JvmOverloads
+    @JvmStatic
+    fun getChatBreak(separator: String = "-"): String {
+        val len = Renderer.getStringWidth(separator) * Minecraft.getMinecraft().gameSettings.chatScale
+        val times = getChatWidth() / len
+
+        return separator.repeat(times.toInt())
+    }
+
+    @JvmStatic
+    fun centerMessage(message: Any): String = when (message) {
+        // TODO:
+        // is Message ->
+        // is TextComponent ->
+        is String -> {
+            val scale = Minecraft.getMinecraft().gameSettings.chatScale
+            val msgWidth = Renderer.getStringWidth(message) * scale
+            val margins = getChatWidth() - msgWidth
+            val count = margins / Renderer.getStringWidth(" ") / scale
+            " ".repeat(count.toInt())
+        }
+
+        else -> centerMessage(message.toString())
+    }
 }
