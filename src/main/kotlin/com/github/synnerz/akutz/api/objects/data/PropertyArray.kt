68a2c91a8d9293c90d999649d3c40a7f49566f4a
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-19 20:10:24 -0500

    Property: make things mutable (i forgor)

--- src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyArray.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyArray.kt
@@ -1,16 +1,16 @@
 package com.github.synnerz.akutz.api.objects.data
 
 open class PropertyArray<T : AProperty<*>>(val clazz: Class<T>, val validator: T?, initialValue: List<T> = mutableListOf()) :
-    AProperty<List<T>>(initialValue) {
+    AProperty<MutableList<T>>(initialValue.toMutableList()) {
     protected val MAGIC = "OOGABOOGACAVEMANBRAIN"
-    override fun parse(value: String): List<T> {
+    override fun parse(value: String): MutableList<T> {
         val o = validator ?: clazz.getConstructor().newInstance()
-        return value.split(MAGIC).map { o.clone().update(it) } as List<T>
+        return value.split(MAGIC).map { o.clone().update(it) } as MutableList<T>
     }
 
     override fun serialize(): String = get().joinToString(MAGIC, transform = { it.serialize() })
 
-    override fun validate(value: List<T>) {}
+    override fun validate(value: MutableList<T>) {}
 
-    override fun clone(): AProperty<List<T>> = PropertyArray(clazz, validator, get().toMutableList())
+    override fun clone(): AProperty<MutableList<T>> = PropertyArray(clazz, validator, get().toMutableList())
 }
--- src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyObject.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyObject.kt
@@ -1,21 +1,21 @@
 package com.github.synnerz.akutz.api.objects.data
 
 open class PropertyObject(initialValue: Map<String, AProperty<*>> = mutableMapOf()) :
-    AProperty<Map<String, AProperty<*>>>(initialValue) {
+    AProperty<MutableMap<String, AProperty<*>>>(initialValue.toMutableMap()) {
     protected val MAGIC1 = "TREEMANSAIDTHISCODEISSMELLY"
     protected val MAGIC2 = "IWOULDHAVETOAGREEWITHHIMTHISSHITSUCKS"
-    override fun parse(value: String): Map<String, AProperty<*>> = get() + value.split(MAGIC1).associate {
+    override fun parse(value: String): MutableMap<String, AProperty<*>> = (get() + value.split(MAGIC1).associate {
         val split = it.split(MAGIC2)
         split[0] to get()[split[0]]!!.clone().update(split[1])
-    }
+    }).toMutableMap()
 
     override fun serialize(): String = get().toList().joinToString { it.first + MAGIC2 + it.second.serialize() }
 
-    override fun validate(value: Map<String, AProperty<*>>) {
+    override fun validate(value: MutableMap<String, AProperty<*>>) {
         value.forEach { (k, v) -> (get()[k]!! as AProperty<Any>).validate(v) }
     }
 
-    override fun clone(): AProperty<Map<String, AProperty<*>>> = PropertyObject(get().toMutableMap())
+    override fun clone(): AProperty<MutableMap<String, AProperty<*>>> = PropertyObject(get().toMutableMap())
 
-    override fun set(v: Map<String, AProperty<*>>) = throw UnsupportedOperationException("cant set on an object sorry :(")
+    override fun set(v: MutableMap<String, AProperty<*>>) = throw UnsupportedOperationException("cant set on an object sorry :(")
 }
