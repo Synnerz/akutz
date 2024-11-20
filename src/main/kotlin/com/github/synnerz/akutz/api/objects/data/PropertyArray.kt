ae473cec9140a8923bf76c49e861ae67b502a7f1
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-19 20:16:27 -0500

    mmm getters

--- src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyArray.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyArray.kt
@@ -13,4 +13,15 @@ open class PropertyArray<T : AProperty<*>>(val clazz: Class<T>, val validator: T
     override fun validate(value: MutableList<T>) {}
 
     override fun clone(): AProperty<MutableList<T>> = PropertyArray(clazz, validator, get().toMutableList())
+
+    open fun getArray(key: Int) = get()[key] as PropertyArray<*>
+    open fun getBoolean(key: Int) = get()[key] as PropertyBoolean
+    open fun getColor(key: Int) = get()[key] as PropertyColor
+    open fun getDouble(key: Int) = get()[key] as PropertyDouble
+    open fun getFlags(key: Int) = get()[key] as PropertyFlags
+    open fun getInteger(key: Int) = get()[key] as PropertyInteger
+    open fun getObject(key: Int) = get()[key] as PropertyObject
+    open fun getOption(key: Int) = get()[key] as PropertyOption
+    open fun getPercent(key: Int) = get()[key] as PropertyPercent
+    open fun getString(key: Int) = get()[key] as PropertyString
 }
--- src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyObject.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/data/PropertyObject.kt
@@ -17,5 +17,17 @@ open class PropertyObject(initialValue: Map<String, AProperty<*>> = mutableMapOf
 
     override fun clone(): AProperty<MutableMap<String, AProperty<*>>> = PropertyObject(get().toMutableMap())
 
-    override fun set(v: MutableMap<String, AProperty<*>>) = throw UnsupportedOperationException("cant set on an object sorry :(")
+    override fun set(v: MutableMap<String, AProperty<*>>) =
+        throw UnsupportedOperationException("cant set on an object sorry :(")
+
+    open fun getArray(key: String) = get()[key] as PropertyArray<*>
+    open fun getBoolean(key: String) = get()[key] as PropertyBoolean
+    open fun getColor(key: String) = get()[key] as PropertyColor
+    open fun getDouble(key: String) = get()[key] as PropertyDouble
+    open fun getFlags(key: String) = get()[key] as PropertyFlags
+    open fun getInteger(key: String) = get()[key] as PropertyInteger
+    open fun getObject(key: String) = get()[key] as PropertyObject
+    open fun getOption(key: String) = get()[key] as PropertyOption
+    open fun getPercent(key: String) = get()[key] as PropertyPercent
+    open fun getString(key: String) = get()[key] as PropertyString
 }
