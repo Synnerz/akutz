8e4390c85dc9750ec7db7642aaf6e84297ab22a8
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-20 21:54:02 -0700

    AkutzCommand: better messages +commands

--- src/main/kotlin/com/github/synnerz/akutz/command/AkutzCommand.kt
+++ src/main/kotlin/com/github/synnerz/akutz/command/AkutzCommand.kt
@@ -1,11 +1,13 @@
 package com.github.synnerz.akutz.command
 
+import com.github.synnerz.akutz.Akutz
 import com.github.synnerz.akutz.api.commands.BaseCommand
 import com.github.synnerz.akutz.api.libs.ChatLib
 import com.github.synnerz.akutz.api.objects.data.PersistentData
 import com.github.synnerz.akutz.engine.impl.Impl
 import com.github.synnerz.akutz.engine.module.ModuleManager
 import net.minecraft.client.entity.EntityPlayerSP
+import java.awt.Desktop
 
 object AkutzCommand : BaseCommand("Akutz", listOf("akutz", "az", "akz")) {
     override fun processCommand(player: EntityPlayerSP, args: Array<String>) {
@@ -19,25 +21,41 @@ object AkutzCommand : BaseCommand("Akutz", listOf("akutz", "az", "akz")) {
                 ModuleManager.setup()
                 Impl.setup()
                 ModuleManager.start()
-                ChatLib.chat("Akutz was loaded")
+                ChatLib.chat("§b§lAkutz§r: §bLoaded modules.")
             }
             "unload" -> {
-                if (!Impl.isLoaded()) return ChatLib.chat("Akutz has already been unloaded")
+                if (!Impl.isLoaded()) return ChatLib.chat("§b§lAkutz§r: §cModules have already been unloaded.")
 
                 PersistentData.INSTANCES.forEach{ it.save() }
                 PersistentData.INSTANCES.clear()
                 Impl.shutdown()
-                ChatLib.chat("Akutz was unloaded")
+                ChatLib.chat("§b§lAkutz§r: §cUnloaded modules.")
+            }
+            "file", "files" -> {
+                try {
+                    Desktop.getDesktop().open(Akutz.configLocation)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            "import" -> {
+                // TODO: uncomment whenever we recode ModuleManager or something
+                // if (args.getOrNull(1).isNullOrEmpty()) return ChatLib.chat("§b§lAkutz§r: §cPlease specify a module name")
+                // ModuleManager.import(args[1])
+                ChatLib.chat("§b§lAkutz§r: §cCommand currently disabled.")
             }
             else -> ChatLib.chat(getHelp())
         }
     }
 
     private fun getHelp() = """
-        Akutz: the BETTER minecraft javascript framework
-        Aliases: akutz, az, akz
-        /akutz load - Loads the modules in the folder
-        /akutz unload - Unloads the modules that were loaded
-        /akutz reload - (Alias for /akutz load does the same thing)
+        §b§lAkutz§r: §bthe BETTER minecraft javascript framework.
+        §b§lAliases§r: §eakutz, az, akz
+        §a/akutz load §e- §bLoads the modules in the folder.
+        §a/akutz unload §e- §bUnloads the modules that were loaded.
+        §a/akutz reload §e- §b(Alias for /akutz load does the same thing).
+        §a/akutz file§8(s) §e- §bOpens the modules folder.
+        §a/akutz import §e- §7Currently disabled.
+        §a/akutz help §e- §bDisplays this message in chat.
     """.trimIndent()
 }
