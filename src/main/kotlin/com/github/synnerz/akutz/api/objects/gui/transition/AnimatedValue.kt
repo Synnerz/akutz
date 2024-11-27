ef4e62d507905aa5479b9048a2355f6c3a9b2e7f
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-27 18:12:54 -0500

    formatting

--- src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/transition/AnimatedValue.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/transition/AnimatedValue.kt
@@ -1,6 +1,10 @@
 package com.github.synnerz.akutz.api.objects.gui.transition
 
-abstract class AnimatedValue<T> @JvmOverloads constructor(initialValue: T, var animationTimeMS: Long = 0, var easingFunction: (x: Double) -> Double = { it }) {
+abstract class AnimatedValue<T> @JvmOverloads constructor(
+    initialValue: T,
+    var animationTimeMS: Long = 0,
+    var easingFunction: (x: Double) -> Double = { it }
+) {
     protected var oldValue: T = initialValue
     protected var newValue: T = initialValue
     private var startTime: Long = 0
@@ -11,6 +15,7 @@ abstract class AnimatedValue<T> @JvmOverloads constructor(initialValue: T, var a
         if (dt <= 0) return oldValue
         return lerpValue(easingFunction(dt / animationTimeMS.toDouble()))
     }
+
     open fun set(value: T) {
         if (value == newValue) return
         oldValue = newValue
