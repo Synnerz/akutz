d00b4c7411d361b3c03a4d961844cf6ca36d8fab
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-27 16:52:26 -0500

    ComponentBackground: add ability to style outline

--- src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/GuiComponentBackground.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/GuiComponentBackground.kt
@@ -11,33 +11,50 @@ open class GuiComponentBackground @JvmOverloads constructor(
     p: BaseGuiComponent? = null,
     var bgColor: Color = Color.EMPTY,
     var borderRadius: Double = 0.0,
-    var outlineWidth: Double = 0.0
+    var outlineWidth: Double = 0.0,
+    var outlineStyle: OutlineStyle = OutlineStyle.OUTER,
+    var outlineColor: Color = bgColor.asShade(0.5)
 ) : BaseGuiComponent(x, y, w, h, p) {
     override fun doRender() {
-        Renderer.beginDraw(if (outlineWidth > 0) bgColor.asShade(0.5) else bgColor)
-        if (borderRadius > 0) {
-            Renderer.drawRectangle(cx, cy, cw, ch)
-            if (outlineWidth > 0) {
-                Renderer.color(bgColor)
-                Renderer.drawRectangle(
-                    cx + outlineWidth,
-                    cy + outlineWidth,
-                    cw - outlineWidth * 2,
-                    ch - outlineWidth * 2
-                )
+        Renderer.beginDraw(if (outlineWidth > 0) outlineColor else bgColor)
+
+        if (outlineWidth > 0) {
+            val x = when (outlineStyle) {
+                OutlineStyle.OUTER -> cx - outlineWidth
+                OutlineStyle.INNER -> cx + outlineWidth
             }
-        } else {
-            Renderer.drawRoundRectangle(cx, cy, cw, ch, borderRadius)
-            if (outlineWidth > 0) {
-                Renderer.color(bgColor)
-                Renderer.drawRoundRectangle(
-                    cx + outlineWidth,
-                    cy + outlineWidth,
-                    cw - outlineWidth * 2,
-                    ch - outlineWidth * 2,
-                    borderRadius
-                )
+            val y = when (outlineStyle) {
+                OutlineStyle.OUTER -> cy - outlineWidth
+                OutlineStyle.INNER -> cy + outlineWidth
             }
+            val w = when (outlineStyle) {
+                OutlineStyle.OUTER -> cw + outlineWidth * 2
+                OutlineStyle.INNER -> cw - outlineWidth * 2
+            }
+            val h = when (outlineStyle) {
+                OutlineStyle.OUTER -> ch + outlineWidth * 2
+                OutlineStyle.INNER -> ch - outlineWidth * 2
+            }
+
+            if (borderRadius > 0) Renderer.drawRectangle(x, y, w, h)
+            else Renderer.drawRoundRectangle(x, y, w, h, borderRadius)
+
+            Renderer.color(bgColor)
+        }
+
+        if (borderRadius > 0) Renderer.drawRectangle(cx, cy, cw, ch)
+        else Renderer.drawRoundRectangle(cx, cy, cw, ch, borderRadius)
+    }
+
+    enum class OutlineStyle {
+        OUTER,
+        INNER;
+
+        companion object {
+            @JvmStatic
+            fun getByName(name: String) =
+                OutlineStyle.entries.find { it.name == name.uppercase() }
+                    ?: throw IllegalArgumentException("Unknown parameter: $name")
         }
     }
 }
