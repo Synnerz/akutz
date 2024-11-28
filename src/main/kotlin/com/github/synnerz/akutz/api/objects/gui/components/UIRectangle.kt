04eeb6a106e746c7d64e83019f96277527225556
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-27 17:47:08 -0700

    i might need to fire him

--- src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/components/UIRectangle.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/components/UIRectangle.kt
@@ -25,18 +25,21 @@ open class UIRectangle @JvmOverloads constructor(
             }
 
             OutlineStyle.OUTER -> {
+                Renderer.color(outlineColor)
                 Renderer.drawRoundRectangle(
                     cx - outlineWidth,
                     cy - outlineWidth,
                     cw + outlineWidth * 2,
                     ch + outlineWidth * 2,
-                    borderRadius
+                    borderRadius,
+                    false
                 )
                 Renderer.color(bgColor.get())
                 Renderer.drawRoundRectangle(cx, cy, cw, ch, borderRadius)
             }
 
             OutlineStyle.INNER -> {
+                Renderer.color(outlineColor)
                 Renderer.drawRoundRectangle(cx, cy, cw, ch, borderRadius)
                 Renderer.color(bgColor.get())
                 Renderer.drawRoundRectangle(
@@ -44,7 +47,8 @@ open class UIRectangle @JvmOverloads constructor(
                     cy + outlineWidth,
                     cw - outlineWidth * 2,
                     ch - outlineWidth * 2,
-                    borderRadius
+                    borderRadius,
+                    false
                 )
             }
         }
