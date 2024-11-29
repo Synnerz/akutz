104e0f139b4d455682c4c4e332222db9999396a3
Doc <65479309+DocilElm@users.noreply.github.com>
2024-11-28 22:03:51 -0700

    DraggableGui: coerce values & add setters

--- src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/DraggableGui.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/gui/DraggableGui.kt
@@ -1,5 +1,6 @@
 package com.github.synnerz.akutz.api.objects.gui
 
+import com.github.synnerz.akutz.api.libs.render.Renderer
 import com.github.synnerz.akutz.listeners.MouseListener
 
 class DraggableGui @JvmOverloads constructor(
@@ -7,6 +8,9 @@ class DraggableGui @JvmOverloads constructor(
     var y: Double = 0.0,
     var scale: Double = 1.0
 ) : Gui() {
+    var guiWidth: Double? = null
+    var guiHeight: Double? = null
+
     init {
         onScroll { ls ->
             val dir = ls[2]
@@ -16,9 +20,35 @@ class DraggableGui @JvmOverloads constructor(
 
         MouseListener.registerDraggedListener { deltaX, deltaY, _, _, _ ->
             if (!isOpen()) return@registerDraggedListener
-            x += deltaX
-            y += deltaY
+
+            x = (x + deltaX).coerceIn(0.0, getScreenWidth())
+            y = (y + deltaY).coerceIn(0.0, getScreenHeight())
         }
+    }
+
+    fun setWidth(width: Double) = apply {
+        guiWidth = width
+    }
+
+    fun setHeight(height: Double) = apply {
+        guiHeight = height
+    }
+
+    fun setSize(width: Double, height: Double) = apply {
+        guiWidth = width
+        guiHeight = height
+    }
+
+    private fun getScreenWidth(): Double? {
+        if (guiWidth == null) return Renderer.sr?.scaledWidth_double
+
+        return Renderer.sr?.scaledWidth_double?.minus(guiWidth!!)
+    }
+
+    private fun getScreenHeight(): Double? {
+        if (guiHeight == null) return Renderer.sr?.scaledHeight_double
+
+        return Renderer.sr?.scaledHeight_double?.minus(guiHeight!!)
     }
 
     // TODO: once persistent data api is not shit add auto save and auto load etc
