c1611505854ae26b8ffef22665eb3e5dfd5391b5
Doc <65479309+DocilElm@users.noreply.github.com>
2024-12-15 02:00:40 -0700

    StateVar: make `hooks` internal

--- src/main/kotlin/com/github/synnerz/akutz/api/objects/state/StateVar.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/objects/state/StateVar.kt
@@ -2,7 +2,7 @@ package com.github.synnerz.akutz.api.objects.state
 
 open class StateVar<T>(initialValue: T) : IState<T> {
     private var value = initialValue
-    private val hooks: MutableList<(newValue: T, oldValue: T) -> Unit> = mutableListOf()
+    internal val hooks: MutableList<(T, T) -> Unit> = mutableListOf()
 
     override fun get() = value
 
@@ -13,13 +13,11 @@ open class StateVar<T>(initialValue: T) : IState<T> {
         trigger(oldValue)
     }
 
-    override fun listen(callback: (newValue: T, oldValue: T) -> Unit) {
+    override fun listen(callback: (T, T) -> Unit) {
         hooks.add(callback)
     }
 
     protected fun trigger(oldValue: T) {
-        hooks.forEach { callback ->
-            callback(value, oldValue)
-        }
+        hooks.forEach { it(value, oldValue) }
     }
 }
