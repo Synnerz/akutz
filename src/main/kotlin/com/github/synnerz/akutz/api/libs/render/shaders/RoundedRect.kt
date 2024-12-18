63f4baffbe45123b3416cbc09fdc620bc97bb6ed
Doc <65479309+DocilElm@users.noreply.github.com>
2024-12-17 21:48:45 -0700

    api: add `RoundedRect` shaders

---
+++ src/main/kotlin/com/github/synnerz/akutz/api/libs/render/shaders/RoundedRect.kt
@@ -0,0 +1,30 @@
+package com.github.synnerz.akutz.api.libs.render.shaders
+
+import com.github.synnerz.akutz.api.libs.render.Renderer
+import com.github.synnerz.akutz.shaders.Shader
+import com.github.synnerz.akutz.shaders.uniform.*
+/**
+ * Taken from Elementa under MIT License
+ * [Link](https://github.com/EssentialGG/Elementa/blob/c8cb78334a073ca4554cb74d49a771e5320351c5/src/main/kotlin/club/sk1er/elementa/components/UIRoundedRectangle.kt)
+ */
+object RoundedRect {
+    private var shader: Shader = Shader("ui/rect/rect", "ui/rect/rounded_rect")
+    private var shaderRadiusUniform: FloatUniform = FloatUniform(shader.getUniformLoc("u_Radius"))
+    private var shaderInnerRectUniform: Vec4Uniform = Vec4Uniform(shader.getUniformLoc("u_InnerRect"))
+
+    fun drawRoundedRect(x: Float, y: Float, width: Float, height: Float, radius: Float) {
+        shader.bind()
+
+        shaderRadiusUniform.setValue(radius)
+        shaderInnerRectUniform.setValue(Vector4f(
+            x + radius,
+            y + radius,
+            (x + width) - radius,
+            (y + height) - radius
+        ))
+
+        Renderer.drawRect(x.toDouble(), y.toDouble(), width.toDouble(), height.toDouble())
+
+        shader.unbind()
+    }
+}
