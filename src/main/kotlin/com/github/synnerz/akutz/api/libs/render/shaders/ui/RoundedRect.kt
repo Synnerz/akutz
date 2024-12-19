ea9c96b9411988fe78a2a6521019754b7051999d
Doc <65479309+DocilElm@users.noreply.github.com>
2024-12-18 22:22:16 -0700

    api: refactor `RoundedRect`

--- src/main/kotlin/com/github/synnerz/akutz/api/libs/render/shaders/ui/RoundedRect.kt
+++ src/main/kotlin/com/github/synnerz/akutz/api/libs/render/shaders/ui/RoundedRect.kt
@@ -1,14 +1,17 @@
-package com.github.synnerz.akutz.api.libs.render.shaders
+package com.github.synnerz.akutz.api.libs.render.shaders.ui
 
 import com.github.synnerz.akutz.api.libs.render.Renderer
-import com.github.synnerz.akutz.shaders.Shader
-import com.github.synnerz.akutz.shaders.uniform.*
+import com.github.synnerz.akutz.api.libs.render.shaders.Shader
+import com.github.synnerz.akutz.api.libs.render.shaders.uniform.FloatUniform
+import com.github.synnerz.akutz.api.libs.render.shaders.uniform.Vec4Uniform
+import com.github.synnerz.akutz.api.libs.render.shaders.uniform.Vector4f
+
 /**
  * Taken from Elementa under MIT License
  * [Link](https://github.com/EssentialGG/Elementa/blob/c8cb78334a073ca4554cb74d49a771e5320351c5/src/main/kotlin/club/sk1er/elementa/components/UIRoundedRectangle.kt)
  */
 object RoundedRect {
-    private var shader: Shader = Shader("ui/rect/rect", "ui/rect/rounded_rect")
+    private var shader: Shader = Shader.fromResources("ui/rect/rect", "ui/rect/rounded_rect")
     private var shaderRadiusUniform: FloatUniform = FloatUniform(shader.getUniformLoc("u_Radius"))
     private var shaderInnerRectUniform: Vec4Uniform = Vec4Uniform(shader.getUniformLoc("u_InnerRect"))
 
@@ -20,8 +23,7 @@ object RoundedRect {
             x + radius,
             y + radius,
             (x + width) - radius,
-            (y + height) - radius
-        ))
+            (y + height) - radius))
 
         Renderer.drawRect(x.toDouble(), y.toDouble(), width.toDouble(), height.toDouble())
 
