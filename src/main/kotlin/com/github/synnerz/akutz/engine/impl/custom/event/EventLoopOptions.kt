847c4c85f7df17c0f49b527a883e97a83287d14e
Doc <65479309+DocilElm@users.noreply.github.com>
2025-01-11 23:17:29 -0700

    EventLoopOptions: change to `20` thread pool size

--- src/main/kotlin/com/github/synnerz/akutz/engine/impl/custom/event/EventLoopOptions.kt
+++ src/main/kotlin/com/github/synnerz/akutz/engine/impl/custom/event/EventLoopOptions.kt
@@ -6,10 +6,9 @@ class EventLoopOptions(
     var vopts: VertxOptions
 ) {
     constructor() : this(VertxOptions().setWorkerPoolSize(DEFAULT_THREAD_POOL_SIZE))
-    var gcBeforeClose: Boolean = true
     var pooled: Boolean = false
 
     companion object {
-        const val DEFAULT_THREAD_POOL_SIZE = 4
+        const val DEFAULT_THREAD_POOL_SIZE = 20
     }
 }
